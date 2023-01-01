38fbbff647114c8fa6da99a55b3c8bff0fa89f17
Aslı Sulutaş <cemree.1402@gmail.com>
2023-01-01 21:43:24 +0300

    add components

---
+++ src/components/Quotes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Quotes() {
+  return (
+    <div id="quote-box">
+      <div id="text">
+        <p>lorem30</p>
+      </div>
+      <div id="author">
+        <p>John Doe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Quotes;
