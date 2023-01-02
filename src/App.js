33f7d6fd617191f0ef226a92f7dd658d3ec629fa
Aslı Sulutaş <cemree.1402@gmail.com>
2023-01-02 17:53:04 +0300

    add button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Quotes from "./components/Quotes";
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
       <Quotes />
     </div>
   );
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -9,6 +9,21 @@ function Quotes() {
       <div id="author">
         <p>John Doe</p>
       </div>
+      <div id="buttons">
+        <div className="social-media">
+          <a href="#" id="tweet-quote">
+            <span>
+              <img src="" alt="" />{" "}
+            </span>
+          </a>
+          <a href="#" id="tumblr-quote">
+            <span>
+              <img src="" alt="" />{" "}
+            </span>
+          </a>
+        </div>
+        <button id="new-quote">New Quote</button>
+      </div>
     </div>
   );
 }
